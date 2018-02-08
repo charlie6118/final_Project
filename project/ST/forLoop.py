5e88d39741f054c51d96a053760f6a24f700f910
Charlie  Yu <history660@gmail.com>
2018-02-08 13:17:04 +0000

    Try a bigger range for the loop

--- project/ST/forLoop.py
+++ project/ST/forLoop.py
@@ -5,20 +5,26 @@ import time
 invest = []
 
 
-for symbol in range(1101, 1110):
+for symbol in range(1201, 1237):
     stock_exist = check("02", symbol)
     if not stock_exist:
         pass
     else:
-        print(symbol)
+
         stock = get("02", symbol)
+
+        print('{}之五日均線為{}'.format(symbol, stock.dic["5_days"]))
+        print('{}之二十日均線為{}'.format(symbol, stock.dic["20_days"]))
+        print('{}之六十日均線為{}'.format(symbol, stock.dic["60_days"]))
+
+
         if stock.dic["5_days"] == stock.dic["20_days"] or stock.dic["5_days"] == stock.dic["60_days"] or stock.dic["20_days"] == stock.dic["60_days"]:
-            print(stock.dic["5_days"])
-            print(stock.dic["20_days"])
-            print(stock.dic["60_days"])
             invest.append(symbol)
         else:
             pass
     time.sleep(10)
 
-print(invest)
+if len(invest) != 0:
+    print(invest)
+else:
+    print("nothing match")
